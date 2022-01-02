3c2bd1ee784d54e6ff430d4a37954332d0afce07
Sam Garcia <samgcbusiness@gmail.com>
2022-01-02 13:52:03 +0100

    ✨ Add models

--- app/src/main/java/edu/samgarcia/howstheweather/domain/model/ForecastItem.kt
+++ app/src/main/java/edu/samgarcia/howstheweather/domain/model/ForecastItem.kt
@@ -3,9 +3,8 @@ package edu.samgarcia.howstheweather.domain.model
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class WeatherItem(
+data class ForecastItem(
+    val day: Int,
     val temperature: String,
-    val wind: String,
-    val description: String,
-    val forecast: List<ForecastItem>
+    val wind: String
 )
